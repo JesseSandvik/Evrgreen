0ae3a3cbca12dac2719cac7e0998903b1b7a5ae4
Jesse <sandvikjesse@gmail.com>
2023-05-30 22:50:49 -0400

    added picocli plugin, boilerplate for pico to Main class, accepting command line argument

--- src/main/java/com/blckroot/evrgreen/Main.java
+++ src/main/java/com/blckroot/evrgreen/Main.java
@@ -1,7 +1,22 @@
 package com.blckroot.evrgreen;
 
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Main")
+class Main implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "Hello World!") private String firstArg;
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(firstArg);
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
