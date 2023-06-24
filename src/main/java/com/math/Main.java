0e1e18b599d4437c79f113027bad64b9c896d7ff
Jesse <sandvikjesse@gmail.com>
2023-06-24 00:10:13 -0400

    continued building addition command

--- src/main/java/com/math/Main.java
+++ src/main/java/com/math/Main.java
@@ -2,28 +2,16 @@ package com.math;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
-@Command(name="cmd")
+@Command(name="math", subcommands = {Addition.class})
 public class Main implements Callable<Integer> {
-    @Option(
-            names = {"-a", "--add"},
-            description = "Perform addition."
-    ) private String addElements;
+    char[] operators = { '+', '-', '*', '/'};
 
     @Override
     public Integer call() {
-        int firstInt = Character.getNumericValue(addElements.charAt(0));
-        char operator = addElements.charAt(1);
-        int secondInt = Character.getNumericValue(addElements.charAt(2));
-
-        if (operator == '+') {
-            System.out.println(firstInt + secondInt);
-        }
-
-        return 0;
+        return null;
     }
 
     public static void main(String[] args) {
