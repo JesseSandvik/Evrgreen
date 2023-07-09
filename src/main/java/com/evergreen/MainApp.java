e510c73c51e715f3ba3c2212c62083ebc1ba652d
Jesse Sandvik <sandvikjesse@gmail.com>
2023-07-09 11:03:03 -0400

    established engine for registering and executing plugins from external dir

--- src/main/java/com/evergreen/MainApp.java
+++ src/main/java/com/evergreen/MainApp.java
@@ -1,16 +1,43 @@
 package com.evergreen;
 
+import com.evergreen.plugin.InvalidPluginException;
 import com.evergreen.plugin.PluginContract;
+import com.evergreen.plugin.PluginFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Command(name = "evgr")
 public class MainApp {
-    private List<PluginContract> plugins;
+    private final List<PluginContract> plugins;
+    public MainApp() {
+        plugins = new ArrayList<>();
+    }
+    public void registerPlugin(PluginContract plugin) {
+        plugins.add(plugin);
+    }
+    public void executePlugins() {
+        for (PluginContract plugin : plugins) {
+            plugin.execute();
+        }
+    }
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new MainApp()).execute(args);
+        MainApp app = new MainApp();
+
+        try {
+            String pluginExecutablePath = "somepath";
+            PluginContract plugin = PluginFactory.createPlugin(pluginExecutablePath);
+
+            app.registerPlugin(plugin);
+        } catch (InvalidPluginException e) {
+            throw new RuntimeException(e);
+        }
+
+        app.executePlugins();
+
+        int exitCode = new CommandLine(app).execute(args);
         System.exit(exitCode);
     }
 }
