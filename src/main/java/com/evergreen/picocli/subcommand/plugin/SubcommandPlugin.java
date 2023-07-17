1542df5b1c21a519e3a89e6d220f2f38ee2e5aac
Jesse Sandvik <sandvikjesse@gmail.com>
2023-07-17 08:40:06 -0400

    added subcommandplugin validation for properties file description

--- src/main/java/com/evergreen/picocli/subcommand/plugin/SubcommandPlugin.java
+++ src/main/java/com/evergreen/picocli/subcommand/plugin/SubcommandPlugin.java
@@ -3,8 +3,12 @@ package com.evergreen.picocli.subcommand.plugin;
 import com.evergreen.picocli.subcommand.Subcommand;
 import com.evergreen.plugin.Plugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class SubcommandPlugin extends Subcommand {
     private final Plugin plugin;
@@ -23,6 +27,20 @@ public class SubcommandPlugin extends Subcommand {
     public SubcommandPlugin(String name, String description, List<String> parameters, Map<Character, String> options, Plugin plugin) {
         super(name, description, parameters, options);
         this.plugin = plugin;
+    }
+    public boolean propertiesFileExists(String propertiesFilePath) {
+        File propertiesFile = new File(propertiesFilePath);
+        return propertiesFile.exists();
+    }
+    public boolean propertiesFileHasDescription(String propertiesFilePath) throws IOException {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(propertiesFilePath)) {
+            Properties properties = new Properties();
+            properties.load(input);
+
+            return properties.getProperty("description") != null;
+        } catch (IOException exception) {
+            throw new IOException(exception.getMessage());
+        }
     }
     public Plugin getPlugin() {
         return plugin;
