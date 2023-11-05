3fad718139e55360be5b381ca298db1108286299
Jesse <sandvikjesse@gmail.com>
2023-11-05 18:51:17 -0500

    Command test, Option(s) covered

--- models/command/src/test/java/com/blckroot/evrgreen/models/CommandTest.java
+++ models/command/src/test/java/com/blckroot/evrgreen/models/CommandTest.java
@@ -60,8 +60,8 @@ public class CommandTest {
 
         Command command = new Command("test");
         command.addPositionalParameter(positionalParameter);
-        String actual = command.getPositionalParameters().get(0).getLabel();
 
+        String actual = command.getPositionalParameters().get(0).getLabel();
         assertEquals(expected, actual);
     }
 
@@ -72,8 +72,8 @@ public class CommandTest {
 
         Command command = new Command("test");
         command.addPositionalParameter(positionalParameter);
-        String actual = command.getPositionalParameters().get(0).getSynopsis();
 
+        String actual = command.getPositionalParameters().get(0).getSynopsis();
         assertEquals(expected, actual);
     }
 
@@ -85,8 +85,8 @@ public class CommandTest {
 
         Command command = new Command("test");
         command.addPositionalParameter(positionalParameter);
-        Object actual = command.getPositionalParameters().get(0).getValue();
 
+        Object actual = command.getPositionalParameters().get(0).getValue();
         assertEquals(expected, actual);
     }
 
@@ -98,8 +98,90 @@ public class CommandTest {
 
         Command command = new Command("test");
         command.addPositionalParameter(positionalParameter);
-        Object actual = command.getPositionalParameters().get(0).getValue();
 
+        Object actual = command.getPositionalParameters().get(0).getValue();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_default() {
+        Command command = new Command("test");
+        List<Option> actual = command.getOptions();
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__long_name() {
+        String expected = "--long";
+        Option option = new Option(expected, "");
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        String actual = command.getOptions().get(0).getLongName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__short_name() {
+        String expected = "-short";
+        Option option = new Option("--long", expected, "");
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        String actual = command.getOptions().get(0).getShortName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__synopsis() {
+        String expected = "synopsis";
+        Option option = new Option("--long", "-short", expected);
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        String actual = command.getOptions().get(0).getSynopsis();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__label() {
+        String expected = "label";
+        Option option = new Option("--long", "-short", "synopsis");
+        option.setLabel(expected);
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        String actual = command.getOptions().get(0).getLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__value__string() {
+        String expected = "value";
+        Option option = new Option("--long", "-short", "synopsis");
+        option.setValue(expected);
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        Object actual = command.getOptions().get(0).getValue();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void COMMAND_MODEL__options_add__value__boolean() {
+        boolean expected = true;
+        Option option = new Option("--long", "-short", "synopsis");
+        option.setValue(expected);
+
+        Command command = new Command("test");
+        command.addOption(option);
+
+        Object actual = command.getOptions().get(0).getValue();
         assertEquals(expected, actual);
     }
 }
