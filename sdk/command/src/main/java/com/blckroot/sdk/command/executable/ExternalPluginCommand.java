50af4307acfbeffedd1cd9c7c9fe851451af569f
Jesse Sandvik <sandvikjesse@gmail.com>
2023-11-11 21:06:04 -0500

    build ExternalPlugincommand

--- sdk/command/src/main/java/com/blckroot/sdk/command/executable/ExternalPluginCommand.java
+++ sdk/command/src/main/java/com/blckroot/sdk/command/executable/ExternalPluginCommand.java
@@ -1,10 +1,93 @@
 package com.blckroot.sdk.command.executable;
 
+import com.blckroot.sdk.command.model.Option;
+import com.blckroot.sdk.command.model.PositionalParameter;
+import com.blckroot.sdk.file.system.validator.FileValidator;
+import com.blckroot.sdk.operating.system.service.OperatingSystemService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExternalPluginCommand extends PluginCommand {
     private final String executableFileDirectory;
+
     public ExternalPluginCommand(
             CallableCommand executableCommand, String configurationFileDirectory, String executableFileDirectory) {
         super(executableCommand, configurationFileDirectory);
         this.executableFileDirectory = executableFileDirectory;
+    }
+
+    public String getExecutableFileDirectory() {
+        return executableFileDirectory;
+    }
+
+    private String getExecutableFilePath(String executableFileName) {
+        FileValidator fileValidator = new FileValidator();
+        OperatingSystemService operatingSystemService = new OperatingSystemService();
+        String executableFilePath =
+                executableFileDirectory + operatingSystemService.getFileSeparator() + executableFileName;
+
+        if (!fileValidator.fileExists(executableFilePath) || !fileValidator.fileCanExecute(executableFilePath)) {
+            return null;
+        }
+        return executableFilePath;
+    }
+
+    @Override
+    public Integer call() {
+        String executableFilePath = getExecutableFilePath(this.callableCommand.getName());
+        if (executableFilePath == null) {
+            return 1;
+        }
+
+        List<String> arguments = new ArrayList<>();
+        arguments.add(executableFilePath);
+
+        if (this.callableCommand.getPositionalParameters() != null) {
+            for (PositionalParameter positionalParameter : this.callableCommand.getPositionalParameters()) {
+                if (positionalParameter.getValue() != null) {
+                    arguments.add(positionalParameter.getValue().toString());
+                }
+            }
+        }
+
+        if (this.callableCommand.getOptions() != null) {
+            for (Option option : this.callableCommand.getOptions()) {
+                if (option.getValue() != null) {
+                    arguments.add(option.getLongName());
+                    if (option.getLabel() != null) {
+                        arguments.add(option.getValue().toString());
+                    }
+                }
+            }
+        }
+
+        return executeExternalFile(arguments);
+    }
+
+    private Integer executeExternalFile(List<String> arguments) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command(arguments);
+            Process process = processBuilder.start();
+
+            InputStream inputStream = process.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            process.waitFor();
+            return process.exitValue();
+
+        } catch (RuntimeException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- sdk/command/src/main/java/com/blckroot/sdk/command/executable/PluginCommand.java
+++ sdk/command/src/main/java/com/blckroot/sdk/command/executable/PluginCommand.java
@@ -1,135 +1,14 @@
 package com.blckroot.sdk.command.executable;
 
-import com.blckroot.sdk.command.model.Option;
-import com.blckroot.sdk.command.model.PositionalParameter;
-import com.blckroot.sdk.file.system.service.FileSystemService;
-import com.blckroot.sdk.file.system.validator.FileValidator;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
 public class PluginCommand extends CallableCommandDecorator {
     private final String configurationFileDirectory;
 
-    public PluginCommand(CallableCommand executableCommand, String configurationFileDirectory) {
-        super(executableCommand);
+    public PluginCommand(CallableCommand callableCommand, String configurationFileDirectory) {
+        super(callableCommand);
         this.configurationFileDirectory = configurationFileDirectory;
     }
 
-    @Override
-    public Integer call() {
-        FileValidator fileValidator = new FileValidator();
-        if (!fileValidator.directoryExists(configurationFileDirectory)) {
-            return 1;
-        }
-
-        String configurationFilePath =
-                configurationFileDirectory + this.callableCommand.getName() + ".properties";
-        if (!fileValidator.fileExists(configurationFilePath)) {
-            return 1;
-        }
-
-        FileSystemService fileSystemService = new FileSystemService();
-        Properties commandProperties = fileSystemService.getPropertiesFromFile(configurationFilePath);
-
-        if (commandProperties == null || commandProperties.isEmpty()) {
-            return 1;
-        }
-
-        setCommandProperties(commandProperties);
-        return 0;
-    }
-
-    private void setCommandProperties(Properties properties) {
-        final String VERSION_PROPERTY_KEY = "version";
-        final String SYNOPSIS_PROPERTY_KEY = "synopsis";
-        final String DESCRIPTION_PROPERTY_KEY = "description";
-        final String EXECUTES_WITHOUT_ARGUMENTS_KEY = "executes.without.arguments";
-        final String POSITIONAL_PARAMETER_COUNT_KEY = "positional.parameter.count";
-        final String OPTION_COUNT_KEY="option.count";
-
-        if (!properties.getProperty(VERSION_PROPERTY_KEY).isBlank()) {
-            this.callableCommand.setVersion(properties.getProperty(VERSION_PROPERTY_KEY));
-        }
-
-        if (!properties.getProperty(SYNOPSIS_PROPERTY_KEY).isBlank()) {
-            this.callableCommand.setSynopsis(properties.getProperty(SYNOPSIS_PROPERTY_KEY));
-        }
-
-        if (!properties.getProperty(DESCRIPTION_PROPERTY_KEY).isBlank()) {
-            this.callableCommand.setDescription(properties.getProperty(DESCRIPTION_PROPERTY_KEY));
-        }
-
-        if (!properties.getProperty(EXECUTES_WITHOUT_ARGUMENTS_KEY).isBlank()) {
-            this.callableCommand.setExecutesWithoutArguments(
-                    Boolean.parseBoolean(properties.getProperty(EXECUTES_WITHOUT_ARGUMENTS_KEY)));
-        }
-
-        if (!properties.getProperty(POSITIONAL_PARAMETER_COUNT_KEY).isBlank()) {
-            int positionalParameterCount = Integer.parseInt(properties.getProperty(POSITIONAL_PARAMETER_COUNT_KEY));
-            setPositionalParametersFromProperties(positionalParameterCount, properties);
-        }
-
-        if (!properties.getProperty(OPTION_COUNT_KEY).isBlank()) {
-            int optionCount = Integer.parseInt(properties.getProperty(OPTION_COUNT_KEY));
-            setOptionsFromProperties(optionCount, properties);
-        }
-    }
-
-    private void setPositionalParametersFromProperties(Integer positionalParameterCount, Properties properties) {
-        int index = 1;
-
-        List<PositionalParameter> positionalParameters = new ArrayList<>();
-        while (index < positionalParameterCount) {
-            String POSITIONAL_PARAMETER_LABEL_KEY = index + ".positional.parameter.label";
-            String POSITIONAL_PARAMETER_DESCRIPTION_KEY = index + ".positional.parameter.synopsis";
-            PositionalParameter positionalParameter = new PositionalParameter();
-
-            if (properties.getProperty(POSITIONAL_PARAMETER_LABEL_KEY).isBlank()) {
-                positionalParameter.setLabel(properties.getProperty(POSITIONAL_PARAMETER_LABEL_KEY));
-            }
-
-            if (properties.getProperty(POSITIONAL_PARAMETER_DESCRIPTION_KEY).isBlank()) {
-                positionalParameter.setSynopsis(properties.getProperty(POSITIONAL_PARAMETER_DESCRIPTION_KEY));
-            }
-
-            positionalParameters.add(positionalParameter);
-            index = index + 1;
-        }
-        this.callableCommand.setPositionalParameters(positionalParameters.toArray(new PositionalParameter[0]));
-    }
-
-    private void setOptionsFromProperties(Integer optionCount, Properties properties) {
-        int index = 1;
-
-        List<Option> options = new ArrayList<>();
-        while (index < optionCount) {
-            String OPTION_LONG_NAME_KEY = index + ".option.long.name";
-            String OPTION_SHORT_NAME_KEY = index + ".option.short.name";
-            String OPTION_SYNOPSIS_KEY = index + ".option.synopsis";
-            String OPTION_LABEL_KEY = index + ".option.label";
-            Option option = new Option();
-
-            if (properties.getProperty(OPTION_LONG_NAME_KEY).isBlank()) {
-                option.setLongName(properties.getProperty(OPTION_LONG_NAME_KEY));
-            }
-
-            if (properties.getProperty(OPTION_SHORT_NAME_KEY).isBlank()) {
-                option.setShortName(properties.getProperty(OPTION_SHORT_NAME_KEY));
-            }
-
-            if (properties.getProperty(OPTION_SYNOPSIS_KEY).isBlank()) {
-                option.setSynopsis(properties.getProperty(OPTION_SYNOPSIS_KEY));
-            }
-
-            if (properties.getProperty(OPTION_LABEL_KEY).isBlank()) {
-                option.setLabel(OPTION_LABEL_KEY);
-            }
-
-            options.add(option);
-            index = index + 1;
-        }
-        this.callableCommand.setOptions(options.toArray(new Option[0]));
+    public String getConfigurationFileDirectory() {
+        return configurationFileDirectory;
     }
 }
