160b5d771a6f267864b5fb2d7f18c8ba219cf228
Jesse Sandvik <sandvikjesse@gmail.com>
2023-11-12 03:57:19 -0500

    successfully reading properties from file

--- sdk/command/src/main/java/com/blckroot/sdk/command/Main.java
+++ sdk/command/src/main/java/com/blckroot/sdk/command/Main.java
@@ -4,8 +4,16 @@ import com.blckroot.sdk.command.callable.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        FrameworkBaseCommand command = new LoadPropertiesAtRuntime(new ExecuteProgramAtRuntime(new Test("test")));
+        String propertiesFileDirectory = "command/src/main/resources/";
+        FrameworkBaseCommand command =
+                new LoadPropertiesAtRuntime(new ExecuteProgramAtRuntime(new Test("test")), propertiesFileDirectory);
         int exitCode = command.call();
+
+        System.out.println("VERSION: " + command.getVersion());
+        System.out.println("SYNOPSIS: " + command.getSynopsis());
+        System.out.println("DESCRIPTION: " + command.getDescription());
+        System.out.println("EXECUTES WITHOUT ARGUMENTS: " + command.isExecutesWithoutArguments());
+
         System.exit(exitCode);
     }
 }
--- sdk/command/src/main/java/com/blckroot/sdk/command/callable/LoadPropertiesAtRuntime.java
+++ sdk/command/src/main/java/com/blckroot/sdk/command/callable/LoadPropertiesAtRuntime.java
@@ -1,13 +1,64 @@
 package com.blckroot.sdk.command.callable;
 
-public class LoadPropertiesAtRuntime extends FrameworkCommandDecorator {
+import com.blckroot.sdk.file.system.service.FileSystemService;
+import com.blckroot.sdk.file.system.validator.FileValidator;
 
-    public LoadPropertiesAtRuntime(FrameworkBaseCommand frameworkCommand) {
+import java.util.Properties;
+
+public class LoadPropertiesAtRuntime extends FrameworkCommandDecorator {
+    private final String propertiesFileDirectory;
+
+    public LoadPropertiesAtRuntime(FrameworkBaseCommand frameworkCommand, String propertiesFileDirectory) {
         super(frameworkCommand);
+        this.propertiesFileDirectory = propertiesFileDirectory;
+    }
+
+    private void buildProperties() {
+        String propertiesFilePath =
+                propertiesFileDirectory + super.frameworkCommand.getName() + ".properties";
+
+        System.out.println(propertiesFilePath);
+
+        FileValidator fileValidator = new FileValidator();
+        if (!fileValidator.fileExists(propertiesFilePath)) {
+            System.out.println("This triggered");
+            return;
+        }
+
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(propertiesFilePath);
+
+        if (properties == null || properties.isEmpty()) {
+            return;
+        }
+
+        final String VERSION_PROPERTY_KEY = "version";
+        final String SYNOPSIS_PROPERTY_KEY = "synopsis";
+        final String DESCRIPTION_PROPERTY_KEY = "description";
+        final String EXECUTES_WITHOUT_ARGUMENTS_PROPERTY_KEY = "executes.without.arguments";
+
+        if (!properties.getProperty(VERSION_PROPERTY_KEY).isBlank()) {
+            System.out.println("VERSION: " + properties.getProperty(VERSION_PROPERTY_KEY));
+            this.frameworkCommand.setVersion(properties.getProperty(VERSION_PROPERTY_KEY));
+        }
+
+        if (!properties.getProperty(SYNOPSIS_PROPERTY_KEY).isBlank()) {
+            this.frameworkCommand.setSynopsis(properties.getProperty(SYNOPSIS_PROPERTY_KEY));
+        }
+
+        if (!properties.getProperty(DESCRIPTION_PROPERTY_KEY).isBlank()) {
+            this.frameworkCommand.setDescription(DESCRIPTION_PROPERTY_KEY);
+        }
+
+        if (!properties.getProperty(EXECUTES_WITHOUT_ARGUMENTS_PROPERTY_KEY).isBlank()) {
+            this.frameworkCommand.setExecutesWithoutArguments(
+                    Boolean.parseBoolean(properties.getProperty(EXECUTES_WITHOUT_ARGUMENTS_PROPERTY_KEY)));
+        }
     }
 
     @Override
     public Integer call() throws Exception {
+        buildProperties();
         System.out.println("[ SUCCESSFULLY LOADED PROPERTIES AT RUNTIME ]");
         return super.call();
     }
